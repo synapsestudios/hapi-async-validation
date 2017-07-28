f59c5bf58c5b19fc00ab65d91c34ba90339c7cda
spruce-bruce <aaron@synapsestudios.com>
2017-07-28 14:49:20 -0700

    exporting stuff

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 module.exports = {
   asyncValidation: require('./src/asyncValidation'),
+  ValidationError: require('./src/ValidationError'),
+  bookshelf: {
+    rowExistsWhere: require('./src/bookshelf/row-exists-where'),
+    rowExists: require('./src/bookshelf/row-exists'),
+    rowNotExistsExcept: require('./src/bookshelf/row-not-exists-except'),
+    rowNotExistsWhere: require('./src/bookshelf/row-not-exists-where'),
+    rowNotExists: require('./src/bookshelf/row-not-exists'),
+  }
 }
