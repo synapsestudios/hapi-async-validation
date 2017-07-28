ec2b41f7123e260e7d7ce9c0968846914db6ed48
spruce-bruce <aaron@synapsestudios.com>
2017-07-28 13:58:52 -0700

    clean up row-exists-where

--- src/bookshelf/row-exists-where.js
+++ src/bookshelf/row-exists-where.js
@@ -2,47 +2,38 @@ const Boom = require('boom');
 const get = require('lodash/get');
 const ValidationError = require('../ValidationError');
 
-module.exports = bookshelf => {
-  return (modelName, column, whereColumn, contextValuePath, message, constraintOptions) => {
-    return (value, validatorOptions) => {
-      const options = Object.assign(
-        {
-          convert: true,
-          return404: true,
-          fetchOptions: {},
-        },
-        validatorOptions || {},
-        constraintOptions || {}
-      );
-      return new Promise((resolve, reject) => {
-        const Model = bookshelf.model(modelName);
-        const where = {};
+module.exports = bookshelf => (modelName, column, whereColumn, contextValuePath, message, constraintOptions) => (value, validatorOptions) => {
+  const options = Object.assign(
+    {
+      convert: true,
+      return404: true,
+      fetchOptions: {},
+    },
+    validatorOptions || {},
+    constraintOptions || {}
+  );
 
-        where[column] = value;
+  const where = {};
+  where[column] = value;
+  const query = bookshelf.model(modelName).where(where);
 
-        const query = Model.where(where);
+  const contextValue = get(options.context, contextValuePath);
+  if (typeof contextValue !== 'undefined') {
+    query.where(whereColumn, '=', contextValue);
+  }
 
-        const contextValue = get(options.context, contextValuePath);
-        if (typeof contextValue !== 'undefined') {
-          query.where(whereColumn, '=', contextValue);
+  return query.fetch(options.fetchOptions)
+    .then(model => {
+      if (! model) {
+        let throwable;
+        if (options.return404) {
+          throwable = Boom.notFound(message || 'Row does not exist');
+        } else {
+          throwable = new ValidationError(message || 'Row does not exist', 'rowExists');
         }
-
-        query.fetch(options.fetchOptions)
-          .then(model => {
-            if (! model) {
-              if (options.return404) {
-                reject(Boom.notFound(message || 'Row does not exist'));
-              } else {
-                reject(new ValidationError(message || 'Row does not exist', 'rowExists'));
-              }
-            } else {
-              resolve(options.convert ? model : value);
-            }
-          }
-        ).catch((err) => {
-          reject(err);
-        });
-      });
-    };
-  };
+        throw throwable;
+      } else {
+        return options.convert ? model : value;
+      }
+    })
 };
