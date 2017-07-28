4d93fe80165ddc9c96001b5aaa557edd1e45c211
spruce-bruce <aaron@synapsestudios.com>
2017-07-28 11:30:28 -0700

    add stub test for row exists

---
+++ src/bookshelf/__tests__/row-exists.js
@@ -0,0 +1,5 @@
+test(`injecting bookshelf doesn't crash`);
+test(`validation succeeds when the row exists`);
+test(`validation fails when the row doesn't exist`);
+test(`error returned is a 404 when return404 is true`);
+test('value is untouched if convert option is false');
