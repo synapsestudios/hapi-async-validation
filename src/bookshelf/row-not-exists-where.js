b931f6d05a4ba6025300f66ff8d07509c62444e3
spruce-bruce <aaron@synapsestudios.com>
2017-07-28 14:33:23 -0700

    clean up row-not-exists-where

--- src/bookshelf/row-not-exists-where.js
+++ src/bookshelf/row-not-exists-where.js
@@ -1,34 +1,22 @@
 const get = require('lodash/get');
 const ValidationError = require('../ValidationError');
 
-module.exports = (bookshelf) => {
-  return (modelName, column, whereColumn, contextValuePath, message) => {
-    return (value, options) => {
-      return new Promise((resolve, reject) => {
-        var Model = bookshelf.model(modelName);
-        var where = {};
+module.exports = bookshelf => (modelName, column, whereColumn, contextValuePath, message) => (value, options) => {
+  var where = {};
+  where[column] = value;
+  var query = bookshelf.model(modelName).where(where);
 
-        where[column] = value;
+  const contextValue = get(options.context, contextValuePath);
+  if (typeof contextValue !== 'undefined') {
+    query.where(whereColumn, '=', contextValue);
+  }
 
-        var query = Model.where(where);
-
-        const contextValue = get(options.context, contextValuePath);
-        if (typeof contextValue !== 'undefined') {
-          query.where(whereColumn, '=', contextValue);
-        }
-
-        query.fetch()
-          .then(model => {
-            if (! model) {
-              resolve(value);
-            } else {
-              reject(new ValidationError(message || 'Row exists', 'rowNotExistsWhere'));
-            }
-          }
-          ).catch((err) => {
-            reject(err);
-          });
-      });
-    };
-  };
+  return query.fetch()
+    .then(model => {
+      if (! model) {
+        return value;
+      } else {
+        throw new ValidationError(message || 'Row exists', 'rowNotExistsWhere');
+      }
+    })
 };
