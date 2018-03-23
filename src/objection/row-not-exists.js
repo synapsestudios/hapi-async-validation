d1441614c88a5322dcaf0865808f6583a5d11221
David <david@synapsestudios.com>
2018-03-23 13:51:55 -0700

    Add rowNotExists objection version

--- src/objection/row-not-exists.js
+++ src/objection/row-not-exists.js
@@ -1,12 +1,18 @@
 const ValidationError = require('../ValidationError');
 
-module.exports = bookshelf => (modelName, column, message) => (value, options) => {
-  const where = {};
-  where[column] = value;
+module.exports = Model => (modelName, column, message) => (value, options) => {
 
-  return bookshelf.model(modelName).where(where).fetch()
-    .then(model => {
-      if (! model) {
+  class Table extends Model {
+    static get tableName() {
+      return modelName;
+    }
+  }
+
+  return Table
+    .query()
+    .where(column, '=', value)
+    .then(function(rows){
+      if (rows.length === 0) {
         return value;
       } else {
         throw new ValidationError(message || 'Row exists', 'rowNotExists');
