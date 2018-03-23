b7f5e234e9e93645e88d65d5c7a4db0ed8d8c3e0
David <david@synapsestudios.com>
2018-03-23 16:44:28 -0700

    Prettier pass

--- src/objection/row-exists.js
+++ src/objection/row-exists.js
@@ -1,15 +1,12 @@
-const Boom = require("boom");
-const ValidationError = require("../ValidationError");
+const Boom = require('boom');
+const ValidationError = require('../ValidationError');
 
-module.exports = Model => (modelName, column, message, constraintOptions) => (
-  value,
-  validatorOptions
-) => {
+module.exports = Model => (modelName, column, message, constraintOptions) => (value, validatorOptions) => {
   const options = Object.assign(
     {
       convert: true,
       return404: true,
-      fetchOptions: {}
+      fetchOptions: {},
     },
     validatorOptions || {},
     constraintOptions || {}
@@ -22,17 +19,14 @@ module.exports = Model => (modelName, column, message, constraintOptions) => (
   }
 
   return Table.query()
-    .where(column, "=", value)
+    .where(column, '=', value)
     .then(function(rows) {
       if (rows.length === 0) {
         let throwable;
         if (options.return404) {
-          throwable = Boom.notFound(message || "Row does not exist");
+          throwable = Boom.notFound(message || 'Row does not exist');
         } else {
-          throwable = new ValidationError(
-            message || "Row does not exist",
-            "rowExists"
-          );
+          throwable = new ValidationError(message || 'Row does not exist', 'rowExists');
         }
         throw throwable;
       } else {
