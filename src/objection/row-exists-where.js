8714a32a63292a1ea51ae7c257a7df3009a8663f
David <david@synapsestudios.com>
2018-03-23 16:43:45 -0700

    Add rowExistsWhere and whereBuilder fn

--- src/objection/row-exists-where.js
+++ src/objection/row-exists-where.js
@@ -2,29 +2,43 @@ const Boom = require('boom');
 const get = require('lodash.get');
 const ValidationError = require('../ValidationError');
 
-module.exports = bookshelf => (modelName, column, whereColumn, contextValuePath, message, constraintOptions) => (value, validatorOptions) => {
-  const options = Object.assign(
-    {
-      convert: true,
-      return404: true,
-      fetchOptions: {},
-    },
-    validatorOptions || {},
-    constraintOptions || {}
-  );
-
-  const where = {};
-  where[column] = value;
-  const query = bookshelf.model(modelName).where(where);
-
-  const contextValue = get(options.context, contextValuePath);
-  if (typeof contextValue !== 'undefined') {
-    query.where(whereColumn, '=', contextValue);
+function whereBuilder(validatedValue, whereArray, objectionModel) {
+  if (whereArray.length < 2) {
+    objectionModel.where(whereArray[0][0], whereArray[0][1], whereArray[0][2] || validatedValue);
+  } else {
+    whereArray.forEach(function(item, index) {
+      if (item.length === 3) {
+        objectionModel.andWhere(item[0], item[1], item[2]);
+      } else if (item.length === 2) {
+        objectionModel.andWhere(item[0], item[1], validatedValue);
+      } else {
+        throw new Error('Where clause must have at least 2 or 3 values. Please refer to the documentation.');
+      }
+    });
   }
+  return objectionModel;
+}
 
-  return query.fetch(options.fetchOptions)
-    .then(model => {
-      if (! model) {
+module.exports = Model => (modelName, column, whereArr, message, constraintOptions) =>
+  function(value, validatorOptions) {
+    const options = Object.assign(
+      {
+        convert: true,
+        return404: false,
+        fetchOptions: {},
+      },
+      validatorOptions || {},
+      constraintOptions || {}
+    );
+
+    class Table extends Model {
+      static get tableName() {
+        return modelName;
+      }
+    }
+    const currentWhere = whereArr[0];
+    return whereBuilder(value, whereArr, Table.query()).then(function(rows) {
+      if (rows.length === 0) {
         let throwable;
         if (options.return404) {
           throwable = Boom.notFound(message || 'Row does not exist');
@@ -33,7 +47,7 @@ module.exports = bookshelf => (modelName, column, whereColumn, contextValuePath,
         }
         throw throwable;
       } else {
-        return options.convert ? model : value;
+        return options.convert ? rows : value;
       }
-    })
-};
+    });
+  };
