278d0fd760cc605857a590cb657e947fde21712b
David <david@synapsestudios.com>
2018-03-27 10:42:23 -0700

    Add objection rowNotExistsExcept functionality

--- src/objection/row-not-exists-except.js
+++ src/objection/row-not-exists-except.js
@@ -1,22 +1,19 @@
 const get = require('lodash.get');
 const ValidationError = require('../ValidationError');
 
-module.exports = bookshelf => (modelName, column, whereColumn, contextValuePath, message) => (value, options) => {
-  var where = {};
-  where[column] = value;
-  var query = bookshelf.model(modelName).where(where);
+module.exports = (Model, column, whereColumn, contextValuePath, message) => (value, options) => {
+  var query = Model.query().where(column, '=', value);
 
   const contextValue = get(options.context, contextValuePath);
   if (typeof contextValue !== 'undefined') {
     query.where(whereColumn, '!=', contextValue);
   }
 
-  return query.fetch()
-    .then(model => {
-      if (! model) {
-        return value;
-      } else {
-        throw new ValidationError(message || 'Row exists', 'rowNotExistsExcept');
-      }
-    })
+  return query.then(rows => {
+    if (rows.length === 0) {
+      return value;
+    } else {
+      throw new ValidationError(message || 'Row exists', 'rowNotExistsExcept');
+    }
+  });
 };
