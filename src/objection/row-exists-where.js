960064baa55343139f1c65d4a1017ddc3102e003
David <david@synapsestudios.com>
2018-03-27 10:01:39 -0700

    Change rowExistsWhere to be closer to bookshelf version

--- src/objection/row-exists-where.js
+++ src/objection/row-exists-where.js
@@ -1,70 +1,37 @@
-// TODO use actual models that have relations working
 const Boom = require('boom');
 const get = require('lodash.get');
+const merge = require('lodash/merge');
 const ValidationError = require('../ValidationError');
 
-function mapContextValues(whereArray, validatorOptions) {
-  return whereArray.map(function(whereClause, index) {
-    if (whereClause[2].slice(0, 3) !== 'cv:') {
-      return whereClause;
-    } else {
-      const contextValuePath = whereClause[2].slice(3);
-      const contextValueToMap = get(validatorOptions.context, contextValuePath);
-      if (contextValueToMap === undefined) {
-        throw new Error('Context Value is undefined');
-      }
-      whereClause[2] = contextValueToMap;
-      return whereClause;
-    }
-  });
-}
-
-function whereBuilder(validatedValue, whereArray, objectionModel) {
-  if (whereArray.length < 2) {
-    objectionModel.where(whereArray[0][0], whereArray[0][1], whereArray[0][2] || validatedValue);
-  } else {
-    whereArray.forEach(function(item, index) {
-      if (item.length === 3) {
-        objectionModel.andWhere(item[0], item[1], item[2]);
-      } else if (item.length === 2) {
-        objectionModel.andWhere(item[0], item[1], validatedValue);
-      } else {
-        throw new Error('Where clause must have at least 2 or 3 values. Please refer to the documentation.');
-      }
-    });
-  }
-  return objectionModel;
-}
-
-module.exports = Model => (modelName, column, whereArr, message, constraintOptions, contextValuePath) =>
+module.exports = (Model, column, whereArr, contextValuePath, message, constraintOptions) =>
   function(value, validatorOptions) {
-    mapContextValues(whereArr, validatorOptions);
-    const options = Object.assign(
+    const options = merge(
       {
         convert: true,
-        return404: false,
-        fetchOptions: {},
+        return404: true,
+        fetchOptions: { eager: '', eagerOptions: {}, eagerAlgorithm: 'WhereInEagerAlgorithm' },
       },
       validatorOptions || {},
       constraintOptions || {}
     );
+    const contextValue = get(options.context, contextValuePath);
 
-    class Table extends Model {
-      static get tableName() {
-        return modelName;
-      }
-    }
-    return whereBuilder(value, whereArr, Table.query()).then(function(rows) {
-      if (rows.length === 0) {
-        let throwable;
-        if (options.return404) {
-          throwable = Boom.notFound(message || 'Row does not exist');
+    return Model.query()
+      .where(column, '=', contextValue)
+      .eagerAlgorithm(Model[options.fetchOptions.eagerAlgorithm])
+      .eagerOptions(options.fetchOptions.eagerOptions)
+      .eager(options.fetchOptions.eager)
+      .then(function(rows) {
+        if (rows.length === 0) {
+          let throwable;
+          if (options.return404) {
+            throwable = Boom.notFound(message || 'Row does not exist');
+          } else {
+            throwable = new ValidationError(message || 'Row does not exist', 'rowExists');
+          }
+          throw throwable;
         } else {
-          throwable = new ValidationError(message || 'Row does not exist', 'rowExists');
+          return options.convert ? rows : value;
         }
-        throw throwable;
-      } else {
-        return options.convert ? rows : value;
-      }
-    });
+      });
   };
