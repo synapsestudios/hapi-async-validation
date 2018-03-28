fc718a7a3afed84777838ca422d48f26c1fef5ea
David <david@synapsestudios.com>
2018-03-28 16:18:53 -0700

    Add working objection/row-not-exists-except tests

--- __tests__/objection/row-not-exists-except.js
+++ __tests__/objection/row-not-exists-except.js
@@ -1,55 +1,74 @@
-const RowNotExistsExceptFactory = require('../../src/bookshelf/row-not-exists-except');
-const getMockBookshelf = require('../../__mocks__/getMockBookshelf');
+const RowNotExistsExceptFactory = require('../../src/objection/row-not-exists-except');
+const getMockObjection= require('../../__mocks__/getMockObjection');
 const mockOptions = require('../../__mocks__/mockOptions');
 mockOptions.context.params.id = 1;
 
-test(`injecting bookshelf returns a function and doesn't crash`, () => {
-  const RowNotExistsWhere = RowNotExistsExceptFactory(getMockBookshelf().bookshelf);
-  expect(typeof RowNotExistsWhere).toBe('function');
+test(`injecting Model + validation options returns a function and doesn't crash`, () => {
+  const mockObjection = getMockObjection(['value']);
+  const Model = mockObjection.Model
+  const RowNotExistsExcept = RowNotExistsExceptFactory(Model, 'column', 'where-column', 'params.id', 'message', {
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
+  expect(typeof RowNotExistsExcept).toBe('function');
 });
 
-test(`injecting bookshelf + validation options returns a function and doesn't crash`, () => {
-  const RowNotExistsWhere = RowNotExistsExceptFactory(getMockBookshelf().bookshelf);
-  expect(typeof RowNotExistsWhere('model-name', 'column', 'except-column', 'context-value-path', 'message')).toBe('function');
-});
-
-test(`bookshelf methods are called the expected number of times with the right args`, () => {
-  const bookshelfMocks = getMockBookshelf(false);
-  const RowNotExistsWhere = RowNotExistsExceptFactory(bookshelfMocks.bookshelf);
-
+test(`Model methods are called the expected number of times with the right args`, () => {
+  const mockObjection = getMockObjection([]);
+  const Model = mockObjection.Model
+  const RowNotExistsExcept = RowNotExistsExceptFactory(Model, 'column', 'except-column', 'params.id', 'message', {
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
   expect.assertions(7);
-  return RowNotExistsWhere('model-name', 'column', 'except-column', 'params.id', 'message')('value', mockOptions)
+  return RowNotExistsExcept('value', mockOptions)
     .then(value => {
-      const { model, where, fetch } = bookshelfMocks.functions;
-      expect(model.mock.calls.length).toBe(1);
-      expect(model.mock.calls[0][0]).toBe('model-name');
+      const { query, where } = mockObjection.functions;
+      expect(query.mock.calls.length).toBe(1);
 
       expect(where.mock.calls.length).toBe(2);
-      expect(where.mock.calls[0][0]).toEqual({column: 'value'});
+      expect(where.mock.calls[0][0]).toEqual('column');
+      expect(where.mock.calls[0][1]).toEqual('=');
+      expect(where.mock.calls[0][2]).toEqual('value');
       expect(where.mock.calls[1][0]).toBe('except-column');
+      console.log(where.mock.calls[1][2]);
       expect(where.mock.calls[1][2]).toBe(1);
-
-      expect(fetch.mock.calls.length).toBe(1);
     });
 });
 
 test(`validation succeeds when the row doesn't exist`, () => {
-  const bookshelfMocks = getMockBookshelf(false);
-  const RowNotExistsWhere = RowNotExistsExceptFactory(bookshelfMocks.bookshelf);
+  const mockObjection = getMockObjection([]);
+  const Model = mockObjection.Model
+  const RowNotExistsExcept = RowNotExistsExceptFactory(Model, 'column', 'except-column', 'params.id', 'message', {
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
 
   expect.assertions(1)
-  return RowNotExistsWhere('model-name', 'column', 'except-column', 'params.id', 'message')('value', mockOptions)
+  return RowNotExistsExcept('value', mockOptions)
     .then(value => {
       expect(value).toBe('value');
     });
 });
 
 test(`validation fails when the row exists and error is ValidationError`, () => {
-  const bookshelfMocks = getMockBookshelf('return value');
-  const RowNotExistsWhere = RowNotExistsExceptFactory(bookshelfMocks.bookshelf);
+  const mockObjection = getMockObjection(['A value']);
+  const Model = mockObjection.Model
+  const RowNotExistsExcept = RowNotExistsExceptFactory(Model, 'column', 'except-column', 'params.id', 'message', {
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
 
   expect.assertions(4);
-  return RowNotExistsWhere('model-name', 'column', 'except-column', 'params.id', 'message')('value', mockOptions)
+  return RowNotExistsExcept('value', mockOptions)
     .catch(error => {
       expect(error.isBoom).toBeFalsy();
       expect(error.name).toBe('ValidationError');
