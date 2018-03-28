3f32db55547c62a012756b65af345390e7541a10
David <david@synapsestudios.com>
2018-03-27 17:28:59 -0700

    Remove objection reference in index

--- index.js
+++ index.js
@@ -9,11 +9,4 @@ module.exports = {
     rowNotExistsWhere: require('./src/bookshelf/row-not-exists-where'),
     rowNotExists: require('./src/bookshelf/row-not-exists'),
   },
-  objection: {
-    rowExistsWhere: require('./src/objection/row-exists-where'),
-    rowExists: require('./src/objection/row-exists'),
-    rowNotExistsExcept: require('./src/objection/row-not-exists-except'),
-    rowNotExistsWhere: require('./src/objection/row-not-exists-where'),
-    rowNotExists: require('./src/objection/row-not-exists'),
-  }
 }
