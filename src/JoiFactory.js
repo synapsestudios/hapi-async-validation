f2abb7968d6c0cfaa1bd510f1372d168544328b2
David <david@synapsestudios.com>
2018-03-27 17:05:47 -0700

    Add JoiFactory as own module

---
+++ src/JoiFactory.js
@@ -0,0 +1,35 @@
+const Joi = require('joi');
+
+module.exports = function JoiFactory(errorDetails) {
+  const keysFromBadValues = {};
+  const ErrorMessageSchema = {};
+  errorDetails.forEach(detail => {
+    keysFromBadValues[detail.path] = true; // An arbitrary value to get the key in the object.
+    ErrorMessageSchema[detail.path] = extended(detail.message)
+      .ValidationError()
+      .message();
+  });
+  return Joi.validate(keysFromBadValues, ErrorMessageSchema, { abortEarly: false });
+};
+
+const extended = message =>
+  Joi.extend(joi => ({
+    name: 'ValidationError',
+    language: {
+      message: message,
+    },
+    pre(value, state, options) {
+      return value;
+    },
+    rules: [
+      {
+        name: 'message',
+        setup(params) {
+          this._flags.round = true;
+        },
+        validate(params, value, state, options) {
+          return this.createError('ValidationError.message', { v: value }, state, options);
+        },
+      },
+    ],
+  }));
