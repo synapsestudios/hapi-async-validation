7c5dcf5b480f32f2fe35e7fe04c655e0ec8db940
David <david@synapsestudios.com>
2018-03-28 15:42:46 -0700

    Add working objection/row-exists-where tests

--- __tests__/objection/row-exists-where.js
+++ __tests__/objection/row-exists-where.js
@@ -1,55 +1,72 @@
-const RowExistsWhereFactory = require('../../src/bookshelf/row-exists-where');
-const getMockBookshelf = require('../../__mocks__/getMockBookshelf');
+const RowExistsWhereFactory = require('../../src/objection/row-exists-where');
+const getMockObjection= require('../../__mocks__/getMockObjection');
 const mockOptions = require('../../__mocks__/mockOptions');
 mockOptions.context.params.id = 1;
 
-test(`injecting bookshelf returns a function and doesn't crash`, () => {
-  const RowExistsWhere = RowExistsWhereFactory(getMockBookshelf().bookshelf);
+test(`injecting Model + validation options returns a function and doesn't crash`, () => {
+  const Model = getMockObjection(['value']).Model;
+  const RowExistsWhere = RowExistsWhereFactory(Model, 'column', 'where-column', 'context-value-path', 'message');
   expect(typeof RowExistsWhere).toBe('function');
 });
 
-test(`injecting bookshelf + validation options returns a function and doesn't crash`, () => {
-  const RowExistsWhere = RowExistsWhereFactory(getMockBookshelf().bookshelf);
-  expect(typeof RowExistsWhere('model-name', 'column', 'where-column', 'context-value-path', 'message')).toBe('function');
-});
+test(`Model methods are called the expected number of times with the right args`, () => {
+  const mockObjection = getMockObjection(['value']);
+  const Model = mockObjection.Model
+  const RowExistsWhere = RowExistsWhereFactory(Model, 'column', 'where-column', 'params.id', 'message', {
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
 
-test(`bookshelf methods are called the expected number of times with the right args`, () => {
-  const bookshelfMocks = getMockBookshelf('return value');
-  const RowExistsWhere = RowExistsWhereFactory(bookshelfMocks.bookshelf);
-
-  expect.assertions(7);
-  return RowExistsWhere('model-name', 'column', 'where-column', 'params.id', 'message')('value', mockOptions)
+  expect.assertions(11);
+  return RowExistsWhere('value', mockOptions)
     .then(value => {
-      const { model, where, fetch } = bookshelfMocks.functions;
-      expect(model.mock.calls.length).toBe(1);
-      expect(model.mock.calls[0][0]).toBe('model-name');
-
+      const { Model, where, eager, eagerAlgorithm, eagerOptions } = mockObjection.functions;
       expect(where.mock.calls.length).toBe(2);
-      expect(where.mock.calls[0][0]).toEqual({column: 'value'});
+      expect(where.mock.calls[0][0]).toEqual('column');
+      expect(where.mock.calls[0][1]).toEqual('=');
+      expect(where.mock.calls[0][2]).toEqual('value');
       expect(where.mock.calls[1][0]).toBe('where-column');
       expect(where.mock.calls[1][2]).toBe(1);
+      expect(eager.mock.calls.length).toBe(1);
+      expect(eager.mock.calls[0][0]).toBe(('eagervalue'));
+      expect(eagerAlgorithm.mock.calls.length).toBe(1);
+      expect(eagerOptions.mock.calls.length).toBe(1);
+      expect(eagerOptions.mock.calls[0][0]).toEqual({test: 'example'});
 
-      expect(fetch.mock.calls.length).toBe(1);
     });
 });
 
 test(`validation succeeds when the row exists`, () => {
-  const bookshelfMocks = getMockBookshelf('return value');
-  const RowExistsWhere = RowExistsWhereFactory(bookshelfMocks.bookshelf);
+  const mockObjection = getMockObjection(['value']);
+  const Model = mockObjection.Model
+  const RowExistsWhere = RowExistsWhereFactory(Model, 'column', 'where-column', 'params.id', 'message', {
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
 
   expect.assertions(1)
-  return RowExistsWhere('model-name', 'column', 'where-column', 'params.id', 'message')('value', mockOptions)
+  return RowExistsWhere('value', mockOptions)
     .then(value => {
-      expect(value).toBe('return value');
+      expect(value).toEqual(['value']);
     });
 });
 
 test(`validation fails when the row doesn't exist and default error is boom 404`, () => {
-  const bookshelfMocks = getMockBookshelf(false);
-  const RowExistsWhere = RowExistsWhereFactory(bookshelfMocks.bookshelf);
+  const mockObjection = getMockObjection([]);
+  const Model = mockObjection.Model
+  const RowExistsWhere = RowExistsWhereFactory(Model, 'column', 'where-column', 'params.id', 'message', {
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
 
   expect.assertions(2);
-  return RowExistsWhere('model-name', 'column', 'where-column', 'params.id', 'message')('value', mockOptions)
+  return RowExistsWhere('value', mockOptions)
     .catch(error => {
       expect(error.isBoom).toBeTruthy();
       expect(error.output.statusCode).toBe(404);
@@ -57,11 +74,18 @@ test(`validation fails when the row doesn't exist and default error is boom 404`
 });
 
 test(`error returned is a ValidationError when return404 is false`, () => {
-  const bookshelfMocks = getMockBookshelf(false);
-  const RowExistsWhere = RowExistsWhereFactory(bookshelfMocks.bookshelf);
+  const mockObjection = getMockObjection([]);
+  const Model = mockObjection.Model
+  const RowExistsWhere = RowExistsWhereFactory(Model, 'column', 'where-column', 'params.id', 'message', {
+    return404: false,
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
 
   expect.assertions(4);
-  return RowExistsWhere('model-name', 'column', 'where-column', 'params.id', 'message', { return404: false })('value', mockOptions)
+  return RowExistsWhere('value', mockOptions)
     .catch(error => {
       expect(error.isBoom).toBeFalsy();
       expect(error.name).toBe('ValidationError');
@@ -71,12 +95,20 @@ test(`error returned is a ValidationError when return404 is false`, () => {
 });
 
 test('value is untouched if convert option is false', () => {
-  const bookshelfMocks = getMockBookshelf('return value');
-  const RowExistsWhere = RowExistsWhereFactory(bookshelfMocks.bookshelf);
+  const mockObjection = getMockObjection(['value']);
+  const Model = mockObjection.Model
+  const RowExistsWhere = RowExistsWhereFactory(Model, 'column', 'where-column', 'params.id', 'message', {
+    return404: false,
+    convert: false,
+    fetchOptions: {
+      eager: 'eagervalue',
+      eagerOptions: {test: 'example'}
+    }
+  });
 
   expect.assertions(1)
-  return RowExistsWhere('model-name', 'column', 'where-column', 'params.id', 'message', { convert: false })('value', mockOptions)
+  return RowExistsWhere('value', mockOptions)
     .then(value => {
-      expect(value).toBe('value');
+      expect(value).toEqual('value');
     });
 });
